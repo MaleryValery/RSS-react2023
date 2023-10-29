36b68499253d04994d27bad107941678392e7341
Valeriia Melnikov <valery.medvedeva@gmail.com>
2023-10-29 16:04:52 +0200

    feat: update path for search input

--- src/pages/Header.tsx
+++ src/pages/Header.tsx
@@ -1,5 +1,5 @@
 import { Component } from 'react';
-import SearchInput from '../components/SearchInput';
+import SearchInput from '../components/Searchinput/SearchInput';
 
 class Header extends Component<object> {
   render() {
