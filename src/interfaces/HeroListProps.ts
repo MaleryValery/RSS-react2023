c6b9250235b5ca655eaa5351bfa781cd5e248f5d
Valeriia Melnikov <valery.medvedeva@gmail.com>
2023-10-29 23:12:58 +0200

    fix: change cardList to HeroesList

---
+++ src/interfaces/HeroListProps.ts
@@ -0,0 +1,7 @@
+import HeroObject from './Hero';
+
+interface HeroListProps {
+  heroes: HeroObject[];
+}
+
+export default HeroListProps;
--- src/interfaces/ListMovies.ts
+++
@@ -1,8 +0,0 @@
-import ResponseObject from './Movie';
-
-interface ListMovies {
-  text: string;
-  movies: ResponseObject[];
-}
-
-export default ListMovies;
