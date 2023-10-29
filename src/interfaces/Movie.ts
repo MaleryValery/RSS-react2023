ff66075e5dc0f45791a42e9454af263c1eadd3e1
Valeriia Melnikov <valery.medvedeva@gmail.com>
2023-10-29 23:09:00 +0200

    fix: change topic and api, change carts to heroes

--- src/interfaces/Movie.ts
+++
@@ -1,9 +0,0 @@
-interface MovieObject {
-  Title: string;
-  Year: string;
-  imdbID: string;
-  Type: string;
-  Poster: string;
-}
-
-export default MovieObject;
--- src/interfaces/MovieCard.ts
+++
@@ -1,9 +0,0 @@
-import MovieObject from './Movie';
-
-interface MovieCard {
-  key: number;
-  movie: MovieObject;
-  text: string;
-}
-
-export default MovieCard;
