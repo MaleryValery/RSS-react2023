19930f625fce7c5810738b476cb0e5af72e1472b
Valeriia Melnikov <valery.medvedeva@gmail.com>
2023-10-30 01:29:17 +0200

    fix: move header to components

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,19 @@
+import { Component } from 'react';
+import classes from './Header.module.css';
+import ErrorState from '../../interfaces/ErrorState';
+import ErrorProps from '../../interfaces/ErrorProps';
+
+class Header extends Component<ErrorProps, ErrorState> {
+  render() {
+    return (
+      <header className={classes.header}>
+        <h1>Ricky App</h1>
+        <button type="button" onClick={() => new Error()}>
+          Get Error
+        </button>
+      </header>
+    );
+  }
+}
+
+export default Header;
