8490da7945f30783814e7e590f46b12499e0f3d1
Valeriia Melnikov <valery.medvedeva@gmail.com>
2023-10-30 13:59:14 +0200

    fix: remove error btn

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,14 +1,11 @@
 import { Component } from 'react';
 import classes from './Header.module.css';
-import ErrorState from '../../interfaces/ErrorState';
-import ErrorProps from '../../interfaces/ErrorProps';
 
-class Header extends Component<ErrorProps, ErrorState> {
+class Header extends Component {
   render() {
     return (
       <header className={classes.header}>
         <h1>Ricky App</h1>
-        <button type="button">Get Error</button>
       </header>
     );
   }
