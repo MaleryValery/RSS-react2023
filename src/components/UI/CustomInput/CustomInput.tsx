7290f4ccd0d60155f2d320c8623bd1d03081a2f2
Valeriia Melnikov <valery.medvedeva@gmail.com>
2023-11-04 18:34:07 +0200

    feat: add handler for keydown, and value to props

--- src/components/UI/CustomInput/CustomInput.tsx
+++ src/components/UI/CustomInput/CustomInput.tsx
@@ -1,13 +1,15 @@
 import CustomInputProps from './CustomInputProps';
 
 function CustomInput(props: CustomInputProps) {
-  const { placeholder, onChange, className } = props;
+  const { value, placeholder, onChange, className, onKeyDown } = props;
   return (
     <div>
       <input
+        value={value}
         placeholder={placeholder}
         type="text"
         onChange={onChange}
+        onKeyDown={onKeyDown}
         className={className}
       />
     </div>
@@ -15,73 +17,3 @@ function CustomInput(props: CustomInputProps) {
 }
 
 export default CustomInput;
-
-// class CustomInput extends Component<CustomInputProps, CustomInputState> {
-//   constructor(props: CustomInputProps) {
-//     super(props);
-//     this.state = {
-//       inputValue: localStorage.getItem('searchValue') || '',
-//     };
-
-//     this.handleChange = this.handleChange.bind(this);
-//   }
-
-//   public async componentDidMount() {
-//     const { onChange } = this.props;
-//     const search = localStorage.getItem('searchValue');
-//     if (search) {
-//       this.setState({
-//         inputValue: search,
-//       });
-//       onChange(search);
-//     } else {
-//       this.setState({
-//         inputValue: '',
-//       });
-//       onChange();
-//     }
-//   }
-
-//   public componentWillUnmount(): void {
-//     const { inputValue } = this.state;
-//     localStorage.setItem('searchValue', inputValue || '');
-//   }
-
-//   private handleChange(event: ChangeEvent<HTMLInputElement>) {
-//     this.setState({ inputValue: event.target.value });
-//   }
-
-//   private async onSubmit(event: MouseEvent): Promise<void> {
-//     event.preventDefault();
-//     const { onChange } = this.props;
-//     const { inputValue } = this.state;
-//     await onChange(inputValue || '');
-//     localStorage.setItem('searchValue', inputValue || '');
-//   }
-
-//   render() {
-//     const { inputValue } = this.state;
-//     return (
-//       <div className={classes.searchWrapper}>
-//         <input
-//           type="text"
-//           value={inputValue || ''}
-//           className={classes.inputSearch}
-//           onChange={(event: ChangeEvent<HTMLInputElement>) =>
-//             this.handleChange(event)
-//           }
-//         />
-//         <CustomButton
-//           disabled={false}
-//           onClick={async (event?: MouseEvent<HTMLElement>): Promise<void> => {
-//             if (event) this.onSubmit(event);
-//           }}
-//         >
-//           Search
-//         </CustomButton>
-//       </div>
-//     );
-//   }
-// }
-
-// export default CustomInput;
--- src/components/UI/CustomInput/CustomInputProps.ts
+++ src/components/UI/CustomInput/CustomInputProps.ts
@@ -1,10 +1,11 @@
-import { ChangeEvent } from 'react';
+import { ChangeEvent, KeyboardEvent } from 'react';
 
 interface CustomInputProps {
   value: string;
   className?: string;
   placeholder: string;
-  onChange: (event: ChangeEvent<HTMLInputElement>) => Promise<void> | void;
+  onKeyDown?: (event: KeyboardEvent<HTMLInputElement>) => void;
+  onChange?: (event: ChangeEvent<HTMLInputElement>) => Promise<void> | void;
 }
 
 export default CustomInputProps;
