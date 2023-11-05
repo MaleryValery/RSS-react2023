2faacc55d2f1ba0771f12e5f9d15094dde097159
Valeriia Melnikov <valery.medvedeva@gmail.com>
2023-11-05 15:37:06 +0200

    feat: add header and main section to home page, add styles

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,31 @@
+import { Outlet, useLocation, useNavigate } from 'react-router-dom';
+import Header from '../components/Header/Header';
 import MainSection from '../components/MainSection/MainSection';
+import classes from './Home.module.css';
 
 function Home() {
-  return <MainSection />;
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  return (
+    <>
+      <Header />
+      <div className={classes.homeMainWrapper}>
+        <button
+          type="button"
+          className={classes.homeContentWrapper}
+          onClick={() => {
+            if (location.pathname.includes('details')) navigate('/');
+          }}
+        >
+          <MainSection />
+        </button>
+        <div>
+          <Outlet />
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Home;
