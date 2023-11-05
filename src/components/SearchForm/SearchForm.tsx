f9787988da8d233cb37496117dc2131b96c99b25
Valeriia Melnikov <valery.medvedeva@gmail.com>
2023-11-05 15:29:52 +0200

    feat: add styles for form, add trim, change serch param name

--- src/components/SearchForm/SearchForm.tsx
+++ src/components/SearchForm/SearchForm.tsx
@@ -4,6 +4,7 @@ import CustomInput from '../UI/CustomInput/CustomInput';
 import CustomButton from '../UI/CustomBotton/CustomButton';
 import LocalStorageService from '../../utils/LocalStorageService';
 import ISearchFormProps from './ISearchFormProps';
+import classes from './SearchForm.module.css';
 
 function SearchForm(props: ISearchFormProps) {
   const [inputValue, setInputValue] = useState(
@@ -14,18 +15,19 @@ function SearchForm(props: ISearchFormProps) {
   const { updateCardsSection, page } = props;
 
   const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
-    setInputValue(event.target.value);
-    LocalStorageService.setData('searchValue', event.target.value);
+    const value = event.target.value.trim();
+    setInputValue(value);
+    LocalStorageService.setData('searchValue', value);
   };
 
   const onSubmit = async (
     e: MouseEvent | KeyboardEvent<HTMLInputElement>
   ): Promise<void> => {
     e.preventDefault();
-    search.set('searchValue', inputValue);
+    page(1);
+    search.set('name', inputValue);
     search.set('page', '1');
     setSearch(search);
-    page(1);
     setInputValue(inputValue);
     LocalStorageService.setData('searchValue', inputValue || '');
     await updateCardsSection(inputValue);
@@ -38,15 +40,16 @@ function SearchForm(props: ISearchFormProps) {
   };
 
   return (
-    <div className="flex flex-row  gap-2">
+    <div className={classes.serchFormWrapper}>
       <CustomInput
         placeholder='search for "Rick and Morty" characters'
         onChange={handleChange}
         value={inputValue}
         onKeyDown={handleInputSubmit}
-        className="placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-1 pl-1 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-Lime-500 focus:ring-1 sm:text-sm"
+        className={classes.customInput}
       />
       <CustomButton
+        className={classes.customButton}
         disabled={false}
         onClick={async (event?: MouseEvent<HTMLElement>): Promise<void> => {
           if (event) onSubmit(event);
