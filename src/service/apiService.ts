3efa9d0967a0c2bd1bd7dcbca7f6184312ed1827
Valeriia Melnikov <valery.medvedeva@gmail.com>
2023-11-12 19:36:06 +0200

    fix: change ApiServise class to functions

--- src/service/apiService.ts
+++ src/service/apiService.ts
@@ -1,31 +1,24 @@
 import axios from 'axios';
 import IResponseData from './IResponseData';
 import ICardData from '../utils/interfaces/ICardData';
+import { BASE_URL, ENDPOINT } from '../utils/const/const';
 
-class ApiService {
-  private static baseUrl = `https://api.potterdb.com/v1/`;
+export const getCharacters = async (
+  value?: string | null,
+  limit: number = 10,
+  page: number = 1
+): Promise<IResponseData> => {
+  const response = await axios.get(`${BASE_URL}/${ENDPOINT}/`, {
+    params: {
+      'filter[name_cont_any]': value || 'potter',
+      'page[size]': limit,
+      'page[number]': page,
+    },
+  });
+  return response.data;
+};
 
-  private static endpoint = `characters`;
-
-  public static getCharacters = async (
-    value?: string | null,
-    limit: number = 10,
-    page: number = 1
-  ): Promise<IResponseData> => {
-    const response = await axios.get(`${this.baseUrl}/${this.endpoint}/`, {
-      params: {
-        'filter[name_cont_any]': value || 'potter',
-        'page[size]': limit,
-        'page[number]': page,
-      },
-    });
-    return response.data;
-  };
-
-  public static getCharactersById = async (id: string): Promise<ICardData> => {
-    const response = await axios.get(`${this.baseUrl}/${this.endpoint}/${id}`);
-    return response.data.data;
-  };
-}
-
-export default ApiService;
+export const getCharactersById = async (id: string): Promise<ICardData> => {
+  const response = await axios.get(`${BASE_URL}/${ENDPOINT}/${id}`);
+  return response.data.data;
+};
